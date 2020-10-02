016ee494612df0832551bee6040b70d41087466f
zbejzer <70958848+zbejzer@users.noreply.github.com>
2020-10-02 09:36:39 +0200

    Add files via upload

--- Tablica_1.cpp
+++ Tablica_1.cpp
@@ -2,23 +2,40 @@
 
 using namespace std;
 
-int tab[10];
+int n;
+int tab[32767];
 
-void wczytanie() {
-	for(int i=0; i<10; i++) {
+
+void wczytanie(int tablica[]) {
+	for(int i=0; i<n; i++) {
 		cout<<"Podaj "<<i+1<<" liczbe: ";
-		cin>>tab[i];
+		cin>>tablica[i];
 	}
 }
 
 void wypisanie(int tablica[]) {
-	for(int i=0; i<10; i++) {
+	for(int i=0; i<n; i++) {
 		cout<<i+1<<" liczba to: "<<tablica[i]<<endl;
 	}
 }
 
+void zamien(int tablica[]) {
+	int temp[n];
+	for(int i=0; i<n; i++) {
+		temp[i] = tablica[n-1-i];
+	}
+	for(int i=0; i<n; i++) {
+		tab[i] = temp[i];
+	}
+}
+
 int main() {
-	wczytanie();
+	cout<<"Podaj n: ";
+	cin>>n;
+	
+	wczytanie(tab);
+	wypisanie(tab);
+	zamien(tab);
 	wypisanie(tab);
 	
 	cin.sync();
